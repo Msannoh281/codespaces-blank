59c9bbca3586d4843920acc5d99e824d160e8d6f
Msannoh281 <144815991+Msannoh281@users.noreply.github.com>
2025-08-22 12:46:43 +0000

    first commit

---
+++ unit 11/data type.py
@@ -0,0 +1,20 @@
+this is a string
+
+mohammed sannoh
+
+19937671092
+
+this is also a string kiyhffuyufvhxhd
+
+$^&^^*^&$!#@$#@^%
+
+
+# this is a comment this is a message that only only humans can see.
+#it is invisible to the computer
+
+1010
+7282821791991
+1717181882829
+0
+1
+2345
