77b95b9e2febfcb4bceb09bd8d7c3ef870ed67ef
Msannoh281 <144815991+Msannoh281@users.noreply.github.com>
2025-08-25 12:35:38 +0000

    class work

--- unit 11/data type.py
+++ unit 11/data type.py
@@ -1,3 +1,5 @@
+#string - character wrapped in quotations.
+
 this is a string
 
 mohammed sannoh
@@ -9,12 +11,33 @@ this is also a string kiyhffuyufvhxhd
 $^&^^*^&$!#@$#@^%
 
 
+
+
 # this is a comment this is a message that only only humans can see.
 #it is invisible to the computer
 
+#integers - number values that can only be whole numbers.
 1010
 7282821791991
 1717181882829
 0
 1
 2345
+
+#Floats - numberr values that can only be decimal numbers.
+10.10
+23.38924
+0.001
+
+
+# Bloolean - is a value that can be either true or false.
+true
+False
+0
+1
+
+# variables - containers for data
+
+schoolname = 'Boys Latin'
+address = '123 Main Street'
+phone = 2155999999
