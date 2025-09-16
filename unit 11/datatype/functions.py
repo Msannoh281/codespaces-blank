838cca63abf48286852e4e218f6958ff2ae1fc5b
Msannoh281 <144815991+Msannoh281@users.noreply.github.com>
2025-09-16 12:48:14 +0000

    classwork

--- unit 11/datatype/functions.py
+++ unit 11/datatype/functions.py
@@ -1,57 +1,45 @@
-#
+#hoe to create a function that passes data
+
+#Step 1. Create a function definition/ instruction to follow
+def bnbRefund( username, refundAmount):
+   print('Sorry '+ username + ' for your cancellation.')
+   print(' We will ')
 
 
 
 
+#Class activity 1
+#Create a birthday message function.
+#Your function should have two parameters; 
+#1 for name and other for birth date
+#both parameters should concatenate the parmeters
+#Your function should concatebate the parameters
+#with a pre-written strings and form the following message
+#print: My name is (Hom) and my birth day is (june 17).
+
+def birthdate(name, date):
+   print('my name is ')
 
 
 
 
+#creat a function that will convert dollars into pennis.
+#your function should take the dollar amount in as a parameter.
+#your function should print out the following message:
 
+#My {13} dollars is equal to {1300} pennies.
 
+def dollarConverter():
+   pennies = dollar * 100
+   print('My '+ dollar + 's is equal to' + str(pennies) + pennies +
+         
+dollarConverter(1)
 
+# Create a function that will calulate that area of a triangle.
+# Your functions= should take in 2 parmeters 
+# One will represent the length, and the other the width
 
+#Hint: to claculate area of a triangle you must
+# calculate the length * the width.
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-   print("Here is your result! ")
-   print(int(a) + b)
-
-
-
-#create a function for calculation numbers
-def calculate(): 
-    a = input('Please enter a number')
-    b = input("Please enter another number: ")
-    print(int(a)  + int(b))
-    print(int(a)  - int(b))
-    print(int(a)  * int(b))
-    print(int(a)  / int(b)) 
-
-calculate(10,10)
-
-# functions have a way passing without users
-# we can past data through the function parmeters
-# the round brackets at the end of a functions are
-# are called function parameters. parameters are placeholders for expected
-# data
-def shoppingCart(items): #items is the parameter /placeholder
-    print( "You have " + str(items) + ' items in your carts')
-
-# when data is passed in the function definition,
-# it is called an arguement because its using rael data
+def triangel 
