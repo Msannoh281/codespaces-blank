577670db73e72a1ab5dcc1874dd087d86cf30ac4
Msannoh281 <144815991+Msannoh281@users.noreply.github.com>
2025-09-24 12:48:28 +0000

    class work

---
+++ unit 11/unit 2/conditions.py
@@ -0,0 +1,24 @@
+
+
+
+
+# Co
+
+
+
+
+
+ studentCredit = input/"please enter the number of credits you have: ")
+
+ if (int(studentCredit) >= 50):
+    print("congrats! you are eligible to graduate.")
+ else:
+    print("sorry, you have not satisfied the required credit points.")
+
+
+weather = 'sunny'
+
+if weather == "sunny":
+  print("have a great day and bring suglasses")
+elif weather == 'rainning'
+  print("remember to bring an unbrella")
