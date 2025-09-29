6e4355fc4d4bd7366b700b797aaf698c4273bcb6
Msannoh281 <144815991+Msannoh281@users.noreply.github.com>
2025-09-29 12:37:37 +0000

    quiz

--- unit 11/quiz1.py
+++ unit 11/quiz1.py
@@ -13,36 +13,49 @@
 # Please write your responses in complete sentences.
 @@ -15,28 +16,28 @@
 
-#abs() Returns the absolute value of a number, all() Returns True if all items in an iterable object are true
-#and any() Returns True if any item in an iterable object is true
+"input"()
+"print"()
+"int"()
 
 # 2. What data type will the following snippet of code output?
 print(30/ 232) 
+'this will be a float data type'
 
-# Arithmetic will follow snippet of code input
+
 
 # 3.  Identify and name the operator family each of the following
 # 3. Identify and name the operator family each of the following
 # symbols belongs to.
 
 # and
+'and belongs to the logical operator.'
 # ==
+'== belongs to the comparison operators family'
 # >
-
+' also belongs to the comparion operator family'
+'
 # 4. Explain the difference between the = operator and the == operator.
 # Please write your response in complete sentences.
-"the == operator means its comaring if the values,"
-"the = operator means it equals to its answer"
+"the = equal sign is used to assign values to varibles."
+"the = equal sign is used to compare values."
 
 # 5. Write code that takes a user’s input (as a string), 
 # casts it to a float, and prints the result multiplied by 2.
+stringNum = input('please write a string bnumber:')
+#newFloat = float(stringNum)
+# print(newFloat * 2)
+print(float(stringNum) * 2)
 
 # 6. What is the difference between a parameter and an argument?
 # Please write your response in complete sentences.
-
+"A parameter can act a varibale or placeholder inside a functiond definition"
+"AN argument is the value that is set to the function when its called."
 # 7. What is the difference between a function definition 
 # and a function invocation? # Please write your response in 
 # complete sentences.
+
+"A function definition is the instructions on what we want our program to do"
+"the function is value that is set to the function"
 
 # 8. Why are functions useful in programming? Provide at least two reasons 
 # and write your reasons in in complete sentences.
