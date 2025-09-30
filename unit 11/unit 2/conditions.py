e48856f76917f30098ea0a4f7d47949973a6d264
Msannoh281 <144815991+Msannoh281@users.noreply.github.com>
2025-09-30 12:48:31 +0000

    classwork

--- unit 11/unit 2/conditions.py
+++ unit 11/unit 2/conditions.py
@@ -8,7 +8,7 @@
 #elif keyword - same thing as the keywords.
 
 #else keyword - Our default outcome.
- studentCredit = input/"please enter the number of credits you have: ")
+ #studentCredit = input/"please enter the number of credits you have: ")
 
  if (int(studentCredit) >= 50):
     print("congrats! you are eligible to graduate.")
@@ -22,3 +22,20 @@ if weather == "sunny":
   print("have a great day and bring suglasses")
 elif weather == 'rainning'
   print("remember to bring an unbrella")
+
+
+#1
+
+def voteAgeChecker(age):
+  if age >= 18:
+   print("congrats! you are old enough to vote.") 
+  else:
+    print("sorry, you are not enough to vote.")
+
+voteAgeChecker(18)
+
+#2
+
+  def maxNumber():
+  a= input
+  b= 
