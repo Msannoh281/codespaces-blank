bd2d47b92fa7d5206ea67f24e26726e62a6e818f
Msannoh281 <144815991+Msannoh281@users.noreply.github.com>
2025-10-09 12:44:27 +0000

    list class work

---
+++ unit 3/List.py
@@ -0,0 +1,43 @@
+# List - is type of collection system where we can store and
+#organize multiple forms of data.
+
+# we create lists using the square brackets and assigning it to variable.
+example == [1,2,3,4,5,6, 'name','dob','year',True,[1,2,3,4, False, 1.2]]
+
+
+# We use list to structure data in large applications.
+
+ videos = ['cat video', 'music video','dacning video']
+
+# we can manipulate lists to add, remove, sort, etc.
+
+ video.append("coding video")
+
+# creat a function that will add a seasoning to list a food seasoings.
+# Your functions should take in a user input, and should use the append() functions
+# to add the season to the seasoning list,
+
+def seasoningShelf():
+    seasonings = ['Salt','Paper','Paprika','Oergeno']
+    newSeasoning = input("please add a seasoning to this list: ")
+    seasonings.append(newSeasoning)
+    print(seasonings)
+
+seasoningShelf()
+
+def alohoodie();
+    hoodiecolors = ['blue','black','grey','white']
+    removecolor = input('white')
+    seasonings = (remove)
+
+numbers = [100, 20, 203, 3, 5, 1000, 243]
+
+
+def addAndSort(number):
+   numbersList.append(newNumber)
+   numbersList.sort()
+   print(numbersList)
+
+addAndSort(60)
+#add new logic to this function to make so that list prints out
+# in reverse order so instead of least to greats, it is greatest to least.
