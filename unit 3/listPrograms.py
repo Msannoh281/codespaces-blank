074cda7ccdda80e3916f330ce2a98aedbb63c521
Msannoh281 <144815991+Msannoh281@users.noreply.github.com>
2025-10-14 12:48:23 +0000

    class work 10/14

---
+++ unit 3/listPrograms.py
@@ -0,0 +1,10 @@
+def foodsShelf():
+   foods = ['pizza','chicken','baccon','sandwich']
+   
+   print(food[0]) # first item in list
+   print(food[3]) # last item in list
+
+# Replace the second list item with a new food, then print
+food.pop(2) # removing item at index 2
+print(food)
+food.
