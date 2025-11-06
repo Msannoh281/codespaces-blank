624437c15607a0f69a2ee718698dfb4aed09fd8e
Msannoh281 <144815991+Msannoh281@users.noreply.github.com>
2025-11-06 13:47:05 +0000

    forLoop classwork

---
+++ unit 3/forLopps.py
@@ -0,0 +1,49 @@
+
+
+for x in range(5):
+    words = ['Please type ina word']
+    print(words)
+    print('we have completed '+ str(x) +' loop')
+    
+
+
+for x in range(3):
+    print('ture or false: 3 is greater than 2')
+    answer = input()
+    if answer != 'true':
+        print('wrong, try agin')
+        print('atempt: '+ str(x))
+    else:
+        print('great! ')
+        break
+
+
+
+for x in range(5):
+    word =input("please type in a word: ")
+    print(word)
+
+
+list0fstudents =['a','b','c']
+studentThatArePresent = []
+studentThatAreAbsent = []
+
+for student in list0fstudents
+
+
+
+
+#looping through strings
+word = "Python"
+for letter in word:
+    print(letter)
+
+#looping through list of numbers
+shoppingPrices = [3.00, 5.40, 7.20, 9.00]
+total = 0
+tax = 0.7
+
+for items in shoppingPrices:
+    total += items * tax
+
+print(total)
